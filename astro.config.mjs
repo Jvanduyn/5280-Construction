705404450dae633c08f945fb15ea2d5c35959aed
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2026-03-26 20:15:52 -0400

    Updated styling and set up for github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://5280construction.local',
+  site: 'https://Jvanduyn.github.io',
+  base: '/5280-Construction',
 
   vite: {
     plugins: [tailwindcss()],
